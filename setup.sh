08a61fda4d82b3ec6751dfc2a9a45448ade9defd
Takeshi Sakai <s.takeshi16@gmail.com>
2016-04-03 13:45:08 +0900

    output how to setup .bash_profile

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-DOT_FILES=( .tmux.conf .vimrc .bash_profile)
+DOT_FILES=( .tmux.conf .vimrc)
 
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+echo "if you need to synchronize .bash_profile, run rm ~/.bash_profile; ln -s ~/dotfiles/.bash_profile ~/.bash_profile"
 
 # NeoBundle のセットアップ
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
